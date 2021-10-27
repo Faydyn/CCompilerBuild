358a84e02f214563facdf41f2ab131459cee6593
Nils Seitz <nils.seitz@icloud.com>
2021-10-27 02:41:08 +0200

    FILE_READ_STD_OUT works (without respect to keywords)

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 moco : moco.o
-		 cc -o moco moco.o
+	 cc -o moco moco.o
  moco.o : main.c
 	 cc -c main.c -o moco.o
  clean :
--- main.c
+++ main.c
@@ -1,45 +1,41 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
 
+#define MAX_SIZE 16
 #define AMOUNT_KEY 23
+#define bool int
+#define false 0
+#define true 1
 
 // yy-Variables and -Functions are required (see "Vorgaben")
 char *yytext;
 int yylen;
 
-char *KEYWORDS[AMOUNT_KEY];
-enum mode;
-
-void yyerror(char *msg);
-
-void read_from_file(char *path);
-void write_to_file(char *path);
-
-
-char *KEYWORDS[AMOUNT_KEY] = {"ARRAY",
-                              "BEGIN",
-                              "CHAR",
-                              "CONST",
-                              "DO",
-                              "ELSE",
-                              "ELSIF",
-                              "END",
-                              "FOR",
-                              "IF",
-                              "INTEGER",
-                              "MODULE",
-                              "OF",
-                              "PROCEDURE",
-                              "REAL",
-                              "REPEAT",
-                              "RETURN",
-                              "THEN",
-                              "TO",
-                              "TYPE",
-                              "UNTIL",
-                              "VAR",
-                              "WHILE"};
-
+const char *KEYWORDS[AMOUNT_KEY] = {"ARRAY",
+                                    "BEGIN",
+                                    "CHAR",
+                                    "CONST",
+                                    "DO",
+                                    "ELSE",
+                                    "ELSIF",
+                                    "END",
+                                    "FOR",
+                                    "IF",
+                                    "INTEGER",
+                                    "MODULE",
+                                    "OF",
+                                    "PROCEDURE",
+                                    "REAL",
+                                    "REPEAT",
+                                    "RETURN",
+                                    "THEN",
+                                    "TO",
+                                    "TYPE",
+                                    "UNTIL",
+                                    "VAR",
+                                    "WHILE"};
 
 typedef enum mode {
     STD_READ_STD_WRITE = 1,
@@ -47,38 +43,88 @@ typedef enum mode {
     FILE_READ_FILE_WRITE,
 } MODE;
 
-
 void yyerror(char *msg) {
     printf("%s\n", msg);
+    free(yytext);
     exit(EXIT_FAILURE);
 }
 
-void read_from_file(char *path) {
-    FILE *infile;
-    infile = fopen(path, "r");
+void reset_yytext() {
+    for(int i = 0; i < yylen; ++i) yytext[i] = '\0'; // TODO: IS THIS CORRECT??
+    yylen = 0;
+}
+void print_reset_yytext(){
+    printf("%s\n",yytext);
+    reset_yytext();
+}
+
+bool is_keyword() {  // id has at least len 1
+    // TODO: Binary search
+    for (int i = 0; i < AMOUNT_KEY; ++i) {
+        const char *key = KEYWORDS[i];
+        // printf("Key: %s\n", key);
+        // printf("%d\n", yylen);
+        // printf("%lu\n", strlen(key) - 1);
+
+        if (yylen == strlen(key) - 1) {
+            for (int j = 0; j <= yylen; ++j) {  // \0 sign can be ignored here, since has to be identical
+                if (j == yylen) return true;
+                else if (yytext[j] != key[j]) break;
+            }
+        }
+    }
+    return false;
+}
+
+void read_from_file(char *path, MODE mode) {
+    FILE *infile = fopen(path, "r");
     if (infile == NULL) yyerror("error: cannot open file for reading");
 
     char c;
+    bool is_identifier = false;
+    size_t buffer_length = 0;
+
     while ((c = getc(infile)) != EOF) {
-        printf("%c", c);
+        if (!is_identifier) {
+            if (isalpha(c)) {  // start of id
+                is_identifier = true;
+                yytext[yylen++] = c;
+            }
+        } else {
+            if (yylen == MAX_SIZE) { // Buffer full
+                ++buffer_length;
+                print_reset_yytext();
+            } else {
+                if (isalnum(c)) yytext[yylen++] = c; // continue id
+                else {
+                    is_identifier = false;
+                    if (buffer_length) {  // KEYWORDS have to be at start, so no need to check
+                        buffer_length = 0;
+                        print_reset_yytext();
+                    } else {
+                        if(!is_keyword()) print_reset_yytext();
+                        else reset_yytext();
+                    }
+                }
+            }
+        }
+
     }
     fclose(infile);
 }
 
 void write_to_file(char *path) {
-    FILE *outfile;
-    outfile = fopen(path, "w");
+    FILE *outfile = fopen(path, "w");
     if (outfile == NULL) yyerror("error: cannot open file for writing");
 
     fclose(outfile);
 }
 
+
+
 int main(int argc, char **argv) {
-
-    MODE mode;
-    FILE *outfile;
-
-    mode = (MODE) argc;
+    const MODE mode = (MODE) argc;
+    yytext = (char *) calloc(MAX_SIZE, sizeof(char));
 
 // use isdigit(), isalpha(), isalnum()
     switch (mode) {
@@ -88,12 +134,12 @@ int main(int argc, char **argv) {
             break;
 
         case FILE_READ_STD_WRITE:
-            read_from_file(argv[1]);
+            read_from_file(argv[1], mode);
 
             break;
 
         case FILE_READ_FILE_WRITE:
-
+            read_from_file(argv[1], mode);
 
             printf("%s\n", argv[0]);
             printf("%s\n", argv[1]);
@@ -104,8 +150,25 @@ int main(int argc, char **argv) {
             yyerror("error: wrong number of parameters");
     }
 
-
+    free(yytext);
     return 0;
 }
-
+/*
+ *
+ MODULE KW
+ HelloWorld
+ TYPE KW
+ myInt
+ INTEGER  X KW
+ CONST KW
+ pi
+ VAR KW
+ x
+ REAL KW
+ BEGIN KW
+ f
+ pi
+ END
+ HelloWorld
+ */
 
