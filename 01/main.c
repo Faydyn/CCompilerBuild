bf8076d68de2d4554ef6611e1bb15a88782a5836
Nils Seitz <nils.seitz@icloud.com>
2021-10-27 21:46:54 +0200

    01/main good now

--- 01/main.c
+++ 01/main.c
@@ -9,7 +9,6 @@
 #define false 0
 #define true 1
 
-#define KEYWORDS_ENABLED 0
 
 // yy-Variables and -Functions are required (see "Vorgaben")
 char *yytext;
@@ -45,185 +44,102 @@ typedef enum mode {
     FILE_READ_FILE_WRITE,
 } MODE;
 
-void yyerror(char *msg) {
+void yyerror(const char *msg) {
     printf("%s\n", msg);
     free(yytext);
     exit(EXIT_FAILURE);
 }
 
-void reset_yytext() {
-    for (int i = 0; i < yylen; ++i) yytext[i] = '\0'; // TODO: IS THIS CORRECT??
+void reset_yy() {
+    for (int i = 0; i < yylen; ++i) yytext[i] = '\0'; // could use MAX_SIZE for safety instead yylen
     yylen = 0;
 }
 
-void print_reset_yytext(const char *eol) {
+bool is_keyword() {  // yytext has at least len 1
+    int low = 0;
+    int mid = AMOUNT_KEY / 2;
+    int high = AMOUNT_KEY;
+    int compare;
+    int *ptr_high_low;
 
-    printf("%s%s", yytext, eol);
-    reset_yytext();
-}
+    while (true) {
+        if (mid == low || mid == high) break;
 
-bool is_keyword() {  // id has at least len 1
-    // TODO: Binary search
-    for (int i = 0; i < AMOUNT_KEY; ++i) {
-        const char *key = KEYWORDS[i];
-        // printf("Key: %s\n", key);
-        // printf("%d\n", yylen);
-        // printf("%lu\n", strlen(key) - 1);
-
-        if (yylen == strlen(key - KEYWORDS_ENABLED)) {
-            for (int j = 0; j <= yylen; ++j) {  // \0 sign can be ignored here, since has to be identical
-                if (j == yylen) return true;
-                else if (yytext[j] != key[j]) break;
-            }
-        }
+        compare = strcmp(yytext, KEYWORDS[mid]);
+        if (compare != 0) {
+            ptr_high_low = (compare < 0) ? &high : &low;
+            *ptr_high_low = mid;  // save to the direct address of either high or low
+            mid = (low + high) / 2;
+        } else return true;
     }
     return false;
 }
 
-void read_from_std() {
+void generic_flush(const char *end, FILE *outfile) {
+    if (outfile) fprintf(outfile, "%s%s", yytext, end);
+    else printf("%s%s", yytext, end);
+    reset_yy();
+}
 
+void generic_read_write(FILE *infile, FILE *outfile) {
     char c;
     bool is_identifier = false;
     size_t buffer_length = 0;
 
-    while ((c = getchar()) != EOF) {
-        if (!is_identifier) {
-            if (isalpha(c)) {  // start of id (GUARD)
+    while ((c = (infile ? getc(infile) : getchar())) != EOF) {
+        if (yylen == MAX_SIZE) { // Buffer full (GUARD)
+            ++buffer_length;
+            generic_flush("", outfile);
+        }
+
+        if (is_identifier) {  // "usual" state
+            if (isalnum(c)) yytext[yylen++] = c; // continue id
+            else {
+                is_identifier = false;
+                if (buffer_length) {  // KEYWORDS have to be at start, so no need to check
+                    buffer_length = 0;
+                    generic_flush("\n", outfile);
+                } else {
+                    if (!is_keyword()) generic_flush("\n", outfile);
+                    else reset_yy();  // only reset without printing
+                }
+            }
+        } else {
+            if (isalpha(c)) {  // start of identifier
                 is_identifier = true;
                 yytext[yylen++] = c;
             }
-        } else {
-            if (yylen == MAX_SIZE) { // Buffer full (GUARD)
-                ++buffer_length;
-                print_reset_yytext("");  // dont eol here
-            } else {
-                if (isalnum(c)) yytext[yylen++] = c; // continue id
-                else {
-                    is_identifier = false;
-                    if (buffer_length) {  // KEYWORDS have to be at start, so no need to check
-                        buffer_length = 0;
-                        print_reset_yytext("\n");
-                    } else {
-                        if (!is_keyword()) print_reset_yytext("\n");  // dont print keywords (see #define on top)
-                        else reset_yytext();  // only reset without printing
-                    }
-                }
-            }
         }
-
     }
 }
-
-void read_from_file(char *inpath) {
-    FILE *infile = fopen(inpath, "r");
-    if (infile == NULL) yyerror("error: cannot open file for reading");
-
-    char c;
-    bool is_identifier = false;
-    size_t buffer_length = 0;
-
-    while ((c = getc(infile)) != EOF) {
-        if (!is_identifier) {
-            if (isalpha(c)) {  // start of id (GUARD)
-                is_identifier = true;
-                yytext[yylen++] = c;
-            }
-        } else {
-            if (yylen == MAX_SIZE) { // Buffer full (GUARD)
-                ++buffer_length;
-                print_reset_yytext("");  // dont eol here
-            } else {
-                if (isalnum(c)) yytext[yylen++] = c; // continue id
-                else {
-                    is_identifier = false;
-                    if (buffer_length) {  // KEYWORDS have to be at start, so no need to check
-                        buffer_length = 0;
-                        print_reset_yytext("\n");
-                    } else {
-                        if (!is_keyword()) print_reset_yytext("\n");  // dont print keywords (see #define on top)
-                        else reset_yytext();  // only reset without printing
-                    }
-                }
-            }
-        }
-
-    }
-    fclose(infile);
-}
-
-void read_write_to_file(char *inpath, char *outpath) {
-    FILE *infile = fopen(inpath, "r");
-    if (infile == NULL) yyerror("error: cannot open file for reading");
-
-    FILE *outfile = fopen(outpath, "a");
-    if (outfile == NULL) yyerror("error: cannot open file for writing");
-
-    char c;
-    bool is_identifier = false;
-    size_t buffer_length = 0;
-
-    while ((c = getc(infile)) != EOF) {
-        if (!is_identifier) {
-            if (isalpha(c)) {  // start of id (GUARD)
-                is_identifier = true;
-                yytext[yylen++] = c;
-            }
-        } else {
-            if (yylen == MAX_SIZE) { // Buffer full (GUARD)
-                ++buffer_length;
-                fprintf(outfile, "%s", yytext);
-                reset_yytext();
-            } else {
-                if (isalnum(c)) yytext[yylen++] = c; // continue id
-                else {
-                    is_identifier = false;
-                    if (buffer_length) {  // KEYWORDS have to be at start, so no need to check
-                        buffer_length = 0;
-                        fprintf(outfile, "%s\n", yytext);
-                        reset_yytext();
-                        print_reset_yytext("\n");
-                    } else {
-                        if (!is_keyword()) {
-                            fprintf(outfile, "%s\n", yytext);
-                            reset_yytext();
-                        } else reset_yytext();  // only reset without printing
-                    }
-                }
-            }
-        }
-
-    }
-
-    fclose(infile);
-    fclose(outfile);
-}
-
 
 int main(int argc, char **argv) {
-    const MODE mode = (MODE) argc;
     yytext = (char *) calloc(MAX_SIZE, sizeof(char));
 
-// use isdigit(), isalpha(), isalnum()
-    switch (mode) {
-        case STD_READ_STD_WRITE:
+    if (argc > 3) yyerror("error: wrong number of parameters");
+    const MODE mode = (MODE) argc;
 
-            read_from_std();
-            break;
+    FILE *infile;
+    FILE *outfile;
 
-        case FILE_READ_STD_WRITE:
-            read_from_file(argv[1]);
+    if (mode != STD_READ_STD_WRITE) {  // only no file in std_read
+        infile = fopen(argv[1], "r");
+        if (infile == NULL) yyerror("error: cannot open file for reading");
+    } else infile = NULL;
+    if (mode == FILE_READ_FILE_WRITE) {  // only file to write in file_write
+        outfile = fopen(argv[2], "a");
+        if (outfile == NULL) {
+            if (infile) fclose(infile); // close before EXIT_FAILURE
+            yyerror("error: cannot open file for writing");
+        }
+    } else outfile = NULL;
 
-            break;
+    generic_read_write(infile, outfile);
 
-        case FILE_READ_FILE_WRITE:
-            read_write_to_file(argv[1], argv[2]);
-            break;
-
-        default:
-            yyerror("error: wrong number of parameters");
-    }
+    if (mode == FILE_READ_FILE_WRITE) fclose(outfile);
+    if (mode != STD_READ_STD_WRITE) fclose(infile);
 
     free(yytext);
-    return 0;
-}
 
+    return EXIT_SUCCESS;
+}
