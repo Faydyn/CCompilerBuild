0728a14a36241c75632a81f2eaa7b0e2ec8badee
Nils Seitz <nils.seitz@icloud.com>
2021-10-27 14:24:39 +0200

    added files for 02 and made Makefile proper

---
+++ 02/src/scanner.c
@@ -0,0 +1,8 @@
+#include <ctype.h>
+#include "../include/buffer.h"
+#include "../include/scanner.h"
+#include "../include/diagnosis.h"
+
+int yylineno = 1;
+
+/* ADD YOUR CODE HERE */
---
+++ 02/src/symboltable.c
@@ -0,0 +1,8 @@
+#include <stdlib.h>
+#include <string.h>
+#include "../include/scanner.h"
+#include "../include/symboltable.h"
+
+symboltable_element* root = NULL;
+
+/* ADD YOUR CODE HERE */
